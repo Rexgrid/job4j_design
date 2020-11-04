f4011c3e67d22cff1500e676132997bbb151cf72
Rexgrid <daervia93@gmail.com>
2020-11-04 13:47:43 +0300

    Реализовать Store<T extends Base> [#359897]

--- chapter_001/src/main/java/ru/job4j/generics/MemStore.java
+++ chapter_001/src/main/java/ru/job4j/generics/MemStore.java
@@ -1,8 +1,8 @@
 package ru.job4j.generics;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Objects;
 
 public final class MemStore<T extends Base> implements Store<T> {
 
@@ -15,22 +15,18 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        for (int i = 0; i < mem.size(); i++) {
-            if (mem.get(i).getId().equals(id)) {
-                mem.set(i, model);
-                return true;
-            }
+        if (indexOf(id) >= 0) {
+            mem.set(indexOf(id), model);
+            return true;
         }
         return false;
     }
 
     @Override
     public boolean delete(String id) {
-        for (int i = 0; i < mem.size(); i++) {
-            if (mem.get(i).getId().equals(id)) {
-                mem.remove(i);
-                return true;
-            }
+        if (indexOf(id) >= 0) {
+            mem.remove(indexOf(id));
+            return true;
         }
         return false;
     }
@@ -41,6 +37,18 @@ public final class MemStore<T extends Base> implements Store<T> {
         for (T t : mem) {
             if (t.getId().equals(id)) {
                 result = t;
+            }
+        }
+        return result;
+    }
+
+    public int indexOf(String id) {
+        int result = 0;
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i).getId().equals(id)) {
+                result = i;
+            } else {
+                result = -1;
             }
         }
         return result;
