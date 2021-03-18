4001b1671180e84b9ded3fc3c42010419fd89b45
Rexgrid <daervia93@gmail.com>
2021-03-18 22:11:46 +0300

    Статистика Коллекций

--- chapter_001/src/main/java/ru/job4j/Collections/Analize.java
+++ chapter_001/src/main/java/ru/job4j/Collections/Analize.java
@@ -6,20 +6,17 @@ import java.util.stream.Collectors;
 public class Analize {
 
     public Info diff(List<User> previous, List<User> current) {
-        Info rsl = new Info();
-        Map<Integer, String> prev = previous.stream().collect(Collectors.toMap(User::getId, User::getName));
-        for (User us : current) {
-            if (prev.containsKey(us.getId()) && !prev.get(us.getId()).equals(us.getName())) {
-                current.remove(us);
+        Info rsl = new Info(0, 0, 0);
+        Map<User, User> cur = current.stream().collect(Collectors.toMap(k -> k, v -> v));
+        for (User prev : previous) {
+            if (cur.get(prev) == null) {
+                rsl.deleted++;
+            } else if (cur.get(prev).getId() == prev.getId() && !cur.get(prev).getName().equals(prev.getName())) {
                 rsl.changed++;
-            } else if (!prev.containsKey(us.getId())) {
-                current.remove(us);
-                rsl.added++;
             }
         }
-        rsl.deleted = current.size();
+        rsl.added = current.size() - previous.size() + rsl.deleted;
         return rsl;
-
     }
 
     public static class User {
@@ -62,6 +59,27 @@ public class Analize {
         int changed;
         int deleted;
 
+        public Info(int added, int changed, int deleted) {
+            this.added = added;
+            this.changed = changed;
+            this.deleted = deleted;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Info info = (Info) o;
+            return added == info.added &&
+                    changed == info.changed &&
+                    deleted == info.deleted;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(added, changed, deleted);
+        }
     }
+
 
 }
--- chapter_001/src/test/java/ru/job4j/Collections/AnalizeTest.java
+++ chapter_001/src/test/java/ru/job4j/Collections/AnalizeTest.java
@@ -2,27 +2,48 @@ package ru.job4j.Collections;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class AnalizeTest {
 
-    @Before
-    public void setUp() {
-        List<Analize.User> cur = List.of(new Analize.User(1, "asd"),
-                new Analize.User(2, "asda"),
-                new Analize.User(3, "asdw"));
+
+    @Test
+    public void whenWeAdded() {
+        List<Analize.User> cur = new ArrayList<>();
+        cur.add(new Analize.User(1, "asd"));
+        cur.add(new Analize.User(2, "asda"));
+        cur.add(new Analize.User(3, "asdw"));
+        List<Analize.User> def = new ArrayList<>();
+        def.add(new Analize.User(1, "asd"));
+        def.add(new Analize.User(2, "asda"));
+        def.add(new Analize.User(3, "asdw"));
+        def.add(new Analize.User(4, "asd"));
+        def.add(new Analize.User(5, "asdq"));
+        Analize analize = new Analize();
+        Analize.Info out = analize.diff(cur, def);
+        Analize.Info expected = new Analize.Info(2,0,0);
+        assertThat(out, is(expected));
     }
 
     @Test
-    public void whenWeTest() {
-        List<Analize.User> def = List.of(
-                new Analize.User(1, "asd"),
-                new Analize.User(2, "asda"),
-                new Analize.User(3, "asdw"),
-                new Analize.User(4, "asd"),
-                new Analize.User(5, "asda"));
-
+    public void whenWeAdd2UsersAndDeleted1() {
+        List<Analize.User> cur = new ArrayList<>();
+        cur.add(new Analize.User(1, "asd"));
+        cur.add(new Analize.User(2, "asda"));
+        cur.add(new Analize.User(3, "asdw"));
+        List<Analize.User> def = new ArrayList<>();
+        def.add(new Analize.User(1, "asd"));
+        def.add(new Analize.User(3, "asdw"));
+        def.add(new Analize.User(4, "asd"));
+        def.add(new Analize.User(5, "asdq"));
+        Analize analize = new Analize();
+        Analize.Info out = analize.diff(cur, def);
+        Analize.Info expected = new Analize.Info(2,0,1);
+        assertThat(out, is(expected));
     }
 }
