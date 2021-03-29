8c93220ca2fa79a16d4a34c3c725f8c3735f920e
Rexgrid <daervia93@gmail.com>
2021-03-29 22:52:41 +0300

    BufferedOutputStream [#359925]

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,8 +22,22 @@ public class LogFilter {
         return rsl;
     }
 
+    public static void save (List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+           for (String lg : log) {
+               out.println(lg);
+           }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
+        save(log, "404.txt");
         log.forEach(System.out::println);
     }
 }
