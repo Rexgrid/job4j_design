f1a380704584582219a6ed3a7404cf93fc905650
Rexgrid <daervia93@gmail.com>
2021-04-07 21:20:09 +0300

    Анализ доступности сервера. [#359930]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -6,29 +6,34 @@ import java.util.*;
 
 
 public class Analizy {
-    public void unavailable(String source, String target) {
-        int innerCount = 0;
-        List<String> rsl = new ArrayList<>();
+    private static List<String> rsl = new ArrayList<>();
+
+    public void unavailable(String source) {
+        boolean serverStatus = true;
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             while (in.read() != -1) {
                 String line = in.readLine();
                 String[] lines = line.split(" ");
-                if (lines[0].equals("400") || lines[0].equals("500")) {
+                if (serverStatus && lines[0].equals("400") || lines[0].equals("500")) {
                     rsl.add(lines[1]);
-                    innerCount++;
-                } else if (innerCount != 0 && lines[0].length() > 0) {
+                    serverStatus = false;
+                } else if (!serverStatus && lines[0].equals("200") || lines[0].equals("300")) {
                     rsl.add(lines[1]);
-                    innerCount = 0;
+                    serverStatus = true;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    public void writeInTarget(List<String> log, String target) {
         try (PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(
                         new FileOutputStream(target)
                 ))) {
-            for (String i : rsl) {
+            for (String i : log) {
                 out.printf("%s;", i);
             }
         } catch (Exception a) {
@@ -36,8 +41,9 @@ public class Analizy {
         }
     }
 
-    public static void main(String[] args) {
-        Analizy al = new Analizy();
-        al.unavailable("unavailable.csv", "test.csv");
+        public static void main(String[] args) {
+            Analizy al = new Analizy();
+            al.unavailable("unavailable.csv");
+            al.writeInTarget(rsl, "test.csv");
+        }
     }
-}
