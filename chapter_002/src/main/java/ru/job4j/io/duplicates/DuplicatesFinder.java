aed0cc731c5c5b9e6aeed8b211b797750e54f96b
Rexgrid <daervia93@gmail.com>
2021-05-19 00:03:40 +0300

    Поиск дубликатов [#359928]

--- chapter_002/src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ chapter_002/src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+
 public class DuplicatesFinder {
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
         Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
     }
 }
--- chapter_002/src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ chapter_002/src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -6,14 +6,22 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private List<Path> list = new ArrayList<>();
+    private Map<String, Long> fp = new HashMap<>();
+    private List<FileProperty> duplicates = new ArrayList<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        FileProperty pr = new FileProperty(file.toFile().length(), file.toFile().getName());
+        if (fp.containsKey(pr.getName()) && fp.containsValue(pr.getSize()))
+        {
+            System.out.println("Найден дубликат файла "+ file.toFile().getName() +" : дубликат находится в директории " + file.toAbsolutePath());
+        }
+        fp.put(pr.getName(), pr.getSize());
         return super.visitFile(file, attrs);
     }
 }
