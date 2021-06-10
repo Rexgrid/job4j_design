9901f3ec5774a5f7946e279216cacab8c7af571f
Rexgrid <daervia93@gmail.com>
2021-06-10 19:54:00 +0300

    доработка метода packFiles

--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -11,26 +11,16 @@ public class Zip {
     public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path item : sources) {
-                   addToZip(zip, item);
+                  zip.putNextEntry(new ZipEntry(item.toFile().getPath()));
+                  try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(item.toFile()))){
+                    zip.write(out.readAllBytes());
+                  }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-   public void addToZip(ZipOutputStream zout, Path dir) throws IOException {
-        File[] nd = dir.toFile().listFiles();
-       for (File itm : nd) {
-           if (itm.isDirectory()) {
-               addToZip(zout, itm.toPath());
-               continue;
-           }
-            zout.putNextEntry(new ZipEntry(itm.getPath()));
-           try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(itm))) {
-               zout.write(out.readAllBytes());
-           }
-       }
-    }
 
     public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
