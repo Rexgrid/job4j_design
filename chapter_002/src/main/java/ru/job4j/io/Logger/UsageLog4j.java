c6e913e163dfcebc11120e6aa64791a541199b94
Rexgrid <daervia93@gmail.com>
2021-06-23 23:22:33 +0300

    Slf4j - вывод переменных

--- chapter_002/src/main/java/ru/job4j/io/Logger/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/Logger/UsageLog4j.java
@@ -10,10 +10,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+      String name = "Alex";
+      int age = 26;
+      double weight = 86.5;
+      float p  = 3.1415926535F;
+      byte two = 0b10;
+      char initials = 'A';
+      boolean wannaLearn = true;
+      long maxValue = 9223372036854775807L;
+      LOG.debug("User info name : {}, age : {}", name, age);
+      LOG.debug("My weight : {}, first character of my name : {}", weight, initials);
+      LOG.debug("What is the number Pi : {}, the bytes 0b10 equal to : {}", p, two);
+      LOG.debug("Do I want to learn java : {}, what is long max value? : {}", wannaLearn, maxValue);
     }
 }
